42c01fe3c8942333c667bca54e22d86d4d1795b7
Alex Quennelle <bluephoenixcg@gmail.com>
2024-09-06 15:27:53 -0500

    Improved player movement +fixed mistakes in level

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,15 +8,27 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpStr = 2f;
     Rigidbody2D rb;
+    BoxCollider2D col;
     bool right, left, jump, grounded;
     float xIn;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
     }
     void Update()
     {
         xIn = Input.GetAxis("Horizontal");
+        if (xIn == 0)
+        {
+            col.sharedMaterial.friction = 1000;
+        }
+        else
+        {
+            col.sharedMaterial.friction = 0;
+        }
+        col.sharedMaterial = col.sharedMaterial;    //What the fuck
+
         if (Input.GetKeyDown(KeyCode.W))
         {
             jump = true;
